dbab2acbd4fe4f604921368f6bcb3ec10fd2f79d
Guy David <gaidavid1@gmail.com>
2024-08-14 02:09:16 +0300

    refactor: 🔥 remove console.log

--- src/handlers/ppsWorkflowHandler.js
+++ src/handlers/ppsWorkflowHandler.js
@@ -1,16 +1,15 @@
 const axios = require("axios");
-const { 
+const {
   DATA_MANAGEMENT_SERVICE_BASE_ADDRS,
-  DATA_MANAGEMENT_SERVICE_PORT
-} = require('../../config/env');
-const { executePpWorkflow } = require('./ppWorkflowHandler');
+  DATA_MANAGEMENT_SERVICE_PORT,
+} = require("../../config/env");
+const { executePpWorkflow } = require("./ppWorkflowHandler");
 
 const ppsWorkflowHandler = async (req, res) => {
   try {
     // Reuse the ppWorkflow logic
     const combinedResponse = await executePpWorkflow(req.body);
 
-    console.log(combinedResponse)
     // Step 4: Store the combined response in the database
     const storeDocumentResponse = await axios.post(
       `${DATA_MANAGEMENT_SERVICE_BASE_ADDRS}:${DATA_MANAGEMENT_SERVICE_PORT}/data-management/document/store`,
@@ -20,7 +19,9 @@ const ppsWorkflowHandler = async (req, res) => {
     return res.status(200).json(storeDocumentResponse.data);
   } catch (error) {
     console.error("Error in pps-workflow:", error.message);
-    return res.status(500).json({ message: "Workflow execution failed", error: error.message });
+    return res
+      .status(500)
+      .json({ message: "Workflow execution failed", error: error.message });
   }
 };
 
