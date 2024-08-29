883b41058523404c757e61c2bbeb28a75561387e
Guy David <gaidavid1@gmail.com>
2024-08-29 22:01:50 +0300

    - Update CORS to accept requests only from the UI, allowing credentials - Reorganize middlewares and eliminate `express.json()` where it is not needed to prevent issues with body parsing

--- apiGateway.js
+++ apiGateway.js
@@ -14,29 +14,48 @@ const predictionServiceProxy = require("./src/proxy/predictionServiceProxy");
 const pPWorkflowHandler = require("./src/handlers/pPWorkflowHandler");
 const pPSWorkflowHandler = require("./src/handlers/pPSWorkflowHandler");
 const dCCWorkflowHandler = require("./src/handlers/dCCWorkflowHandler");
+const cors = require("cors");
+
 
 const app = express();
+// CORS configuration
+app.use(cors({
+  origin: 'http://localhost:3002', 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow standard HTTP methods
+  allowedHeaders: 'Content-Type,Authorization', // Allow specific headers
+  credentials: true, // Allow credentials like cookies
+  exposedHeaders: ['Authorization'], // Expose additional headers if needed
+  maxAge: 600 // Cache the preflight request for 10 minutes
+}));
+app.options('*', cors());
 app.use(cookieParser());
+
+// TODO 
+app.use("/data-management", dataManagementServiceProxy);
+
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(rateLimiter);
 
 // Use authProxy for authentication routes
 app.use("/auth", authServiceProxy);
 
-// Use authentication middleware
-app.use("/image-processing", authenticateToken, imageProcessingServiceProxy);
-app.use("/data-management", authenticateToken, dataManagementServiceProxy);
-app.use("/predictions", authenticateToken, predictionServiceProxy);
+// // Use authentication middleware
+// app.use(authenticateToken);
+
+
+
+app.use("/image-processing", imageProcessingServiceProxy);
+app.use("/predictions", predictionServiceProxy);
 
 //  pp-workflow endpoint
-app.post("/pp-workflow", authenticateToken, pPWorkflowHandler);
-app.post("/pps-workflow",authenticateToken, pPSWorkflowHandler);
-app.post("/dcc-workflow",authenticateToken, dCCWorkflowHandler);
+app.post("/pp-workflow", pPWorkflowHandler);
+app.post("/pps-workflow", pPSWorkflowHandler);
+app.post("/dcc-workflow", dCCWorkflowHandler);
 
 // Use custom error handling middleware
 app.use(errorHandler);
 
 // Start the server
-app.listen(PORT,'0.0.0.0', () => {
+app.listen(PORT, "0.0.0.0", () => {
   logger.info(`API Gateway running on port ${PORT}`);
 });
--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const { AUTH_SERVICE_JWT_SECRET } = require("../../config/env");
+const { AUTH_SERVICE_JWT_SECRET ,REFRESH_AUTH_JWT_SECRET} = require("../../config/env");
 const logger = require("../logger/logger");
 
 const authenticateToken = (req, res, next) => {
@@ -12,12 +12,12 @@ const authenticateToken = (req, res, next) => {
 
   //decoded payload if the signature is valid and optional expiration, audience, or issuer are valid.
   //If not, it will be called with the error.
-  jwt.verify(token, AUTH_SERVICE_JWT_SECRET, (err, user) => {
+  jwt.verify(token, REFRESH_AUTH_JWT_SECRET, (err, user) => {
     if (err) {
       logger.error("Token verification failed", err);
       return res.status(403).json({ message: "Forbidden" });
     }
-
+    
     req.user = user;
     next();
   });
